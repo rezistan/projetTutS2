41115296dfb893a780a6444926de1433545c1c77
TokiDEV <thomasboffy68@gmail.com>
2018-01-01 23:45:31 +0100

    debuts modifs nouveau planning

--- app/Http/Controllers/EdtController.php
+++ app/Http/Controllers/EdtController.php
@@ -21,8 +21,19 @@ class EdtController extends Controller
 {
 
     private $user_activities;
-    private $nb_col;
-    private $nb_row;
+
+    private $previous_year;
+    private $current_year;
+    private $next_year;
+
+    private $previous_week;
+    private $current_week;
+    private $next_week;
+
+    const NB_COLONNE = 6;
+    const NB_HORAIRE = 10;
+    const LASTWEEK = 52;
+    const FIRSTWEEK = 1;
 
 
     /**
@@ -35,24 +46,56 @@ class EdtController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        $this->nb_row = 10;
-        $this->nb_col = 6;
     }
 
     /**
      * Show the basic EDT.
+     * @param $year
+     * @param $week
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($year = -1, $week = 0)
     {
+        if(!isset($year) || !is_numeric($year) || $year < 0) {
+            $year = date("Y");
+        }
+        if(!isset($week) || !is_numeric($week) || $week < self::FIRSTWEEK || $week > self::LASTWEEK) {
+            $week = date("W");
+        }
+
+        $this->current_year = $year;
+        $this->current_week = $week;
+
+        $this->setNextPrev();
+
+        /*$dt = Carbon::now();
+        $dt->year = $year;
+        $dt->weekOfYear = $week;
+        $this->user_activities = \Auth::user()->activities->whereBetween('starded_at', [Carbon::, Carbon::]);*/
         $this->user_activities = \Auth::user()->activities;
         foreach($this->user_activities as $activity) {
             $activity->carbonize();
         }
-        //dd($this->user_activities);
-        //$this->user_activities = $activities->groupBy();
         $planning = $this->getPlanning();
         return view('edt', compact('planning')); // redirige à la vue
+    }
+
+    /**
+     * Il s'agit d'avoir les bonnes dates lorsque l'on clique sur les semaines suivantes et précédentes
+     */
+    private function setNextPrev() {
+        $this->previous_week = $this->current_week - self::FIRSTWEEK;
+        $this->previous_year = $this->current_year;
+        $this->next_week = $this->current_week + self::FIRSTWEEK;
+        $this->next_year = $this->current_year;
+
+        if($this->current_week == self::FIRSTWEEK) {
+            $this->previous_week += self::LASTWEEK;
+            $this->previous_year -= self::FIRSTWEEK;
+        } else if($this->current_week == self::LASTWEEK) {
+            $this->next_week -= self::LASTWEEK;
+            $this->next_year += self::FIRSTWEEK;
+        }
     }
 
     /**
@@ -61,48 +104,51 @@ class EdtController extends Controller
      */
     public function getPlanning()
     {
-        $planning = '<table class="ui center aligned unstackable celled compact definition table">';
-        $planning .= '<thead><tr><th></th><th>Lundi</th><th>Mardi</th><th>Mercredi</th><th>Jeudi</th><th>Vendredi</th></tr></thead>';
+        $planning = '<div id="agenda">';
+        /*$planning .= '<div class="day h-1">Horaires</div>
+                      <div class="day h-1">Lundi</div>
+                      <div class="day h-1">Mardi</div>
+                      <div class="day h-1">Mercredi</div>
+                      <div class="day h-1">Jeudi</div>
+                      <div class="day h-1">Vendredi</div>';*/
 
-        $planning .= '<tbody>';
-        $lastrow = -1;
-        $row = 0;
-        while($row < $this->nb_row) {
-            $planning .= '<tr>';
+        //$lastrow = -1;
+        $planning .= '<div class="day h-1">Horaires';
+        for($row = 0; $row < self::NB_HORAIRE; $row++) {
             $horaire = $row+8;
-            $col = 0;
-            while($col < $this->nb_col) {
+            $planning .= '<div class="day h-1">'.$horaire.'h</div>';
+        }
+        /*for($row = 0; $row < self::NB_HORAIRE; $row++) {
+
+        }*/
+        /*while($col < self::NB_COLONNE) {
                 if($row > $lastrow) {
-                    $planning .= '<td>'.$horaire.'h</td>';
+                    $planning .= '<div class="day h-1">'.$horaire.'h</div>';
                     $lastrow = $row;
                 } else {
                     $found = false;
                     foreach($this->user_activities as $activity) {
                         if($activity->getDay() === $col && $activity->getBeginHour() === $horaire) {
                             $diff = $activity->getEndHour() - $horaire;
-                            $planning .= '<td rowspan="'.$diff.'"><a class="fluid ui simple dropdown '.$activity->task->color.' button">
+                            $planning .= '<div class="day h-'.$diff.'"><a class="fluid ui simple dropdown '.$activity->task->color.' button">
                                           <p class="services">'.$activity->task->task.'</p>
-                                          <p></p> <!-- $activity->users -->
+                                          <p>'.$activity->users->implode('firstname', ', ').'</p>
                                           <p>'.$activity->room->room.'</p>
                                           <div class="menu">
                                               <div class="item"><i class="edit icon"></i> Modifier</div>
                                               <div class="item"><i class="delete icon"></i> Supprimer</div>
                                               <div class="item"><i class="hide icon"></i> Cacher</div>
                                           </div>
-                                       </a></td>';
+                                       </a></div>';
                             $found = true;
                         }
                     }
-                    if(!$found) $planning .= '<td></td>';
+                    if(!$found) $planning .= '<div class="day h-1">rien</div>';
                 }
                 $col++;
-            }
-            $planning .= '</tr>';
-            $row++;
-        }
+            }*/
 
-        $planning .= '</tbody>';
-        $planning .= '</table>';
+        $planning .= '</div>';
 
         return $planning;
     }
